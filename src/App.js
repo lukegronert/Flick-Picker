3cc725e05f7a7fd153882fa15f13369dff36d55d
Luke G <lukegronert@gmail.com>
2022-03-15 13:11:45 +0800

    Created all elements for main page. Need to style, add animation for dropdown, create movie/series display page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import 'normalize.css';
-import 'app.css';
+import './styles/app.css';
+
+import StreamButton from './components/StreamButton';
+import GenerateButton from './components/GenerateButton';
+import AdvancedSettings from './components/AdvancedSettings';
 
 export default function App() {
   return (
     <div className="container">
       <header>
-        <h1>Flick Picker</h1>
+        <h1 className="header">Flick Picker</h1>
       </header>
+      <section>
+        <section className="stream-buttons-section">
+          <StreamButton brand="Netflix"/>
+          <StreamButton brand="Disney"/>
+          <StreamButton brand="Hulu"/>
+          <StreamButton brand="Amazon"/>
+        </section>
+        <section className="generate-button-section">
+          <GenerateButton />
+        </section>
+
+        <AdvancedSettings />
+      </section>
     </div>
   )
 }
